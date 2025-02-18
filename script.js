0f5beddd41dd4e9c755a4b316d9cebd416701005
Lucas Massaroto <102060111+Lucasmassaroto1@users.noreply.github.com>
2025-02-18 12:00:07 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("login-btn").addEventListener("click", function() {
+    window.location.href = "https://discord.com/oauth2/authorize?client_id=SEU_CLIENT_ID&response_type=code&scope=identify";
+});
