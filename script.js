f1eb9b41a159746bb92642ecd232a453a21bed2b
Lucas Massaroto <102060111+Lucasmassaroto1@users.noreply.github.com>
2025-03-05 10:56:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ botao.addEventListener('click', () =>{
     menuLateral.classList.toggle('ativo')
     conteudo.classList.toggle('ativo')
     background.classList.toggle('ativo')
-    document.body.style.backgroundColor = menuLateral.classList.contains('ativo') ? '#34495e' : '#ecf0f1'
+    // document.body.style.backgroundColor = menuLateral.classList.contains('ativo') ? '#34495e' : '#ecf0f1'
 });
 background.addEventListener('click', () =>{
     botao.classList.remove('ativo')
     menuLateral.classList.remove('ativo')
     conteudo.classList.remove('ativo')
     background.classList.remove('ativo')
-    document.body.style.backgroundColor = '#ecf0f1'
+    // document.body.style.backgroundColor = '#ecf0f1'
 })
