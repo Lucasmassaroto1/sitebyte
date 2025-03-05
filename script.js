35c6acb42edd7a3001a6d931f01e172a2efe8b21
Lucas Massaroto <102060111+Lucasmassaroto1@users.noreply.github.com>
2025-03-05 09:59:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-document.getElementById("login-btn").addEventListener("click", function() {
-    window.location.href = "https://discord.com/oauth2/authorize?client_id=SEU_CLIENT_ID&response_type=code&scope=identify";
+const botao = document.querySelector('.toggle');
+const menuLateral = document.querySelector('.menu-lateral');
+const conteudo = document.querySelector('.conteudo');
+const background = document.querySelector('.background');
+const card = document.getElementById('card');
+
+function virar(){
+    card.classList.toggle('virado');
+}
+botao.addEventListener('click', () =>{
+    botao.classList.toggle('ativo')
+    menuLateral.classList.toggle('ativo')
+    conteudo.classList.toggle('ativo')
+    background.classList.toggle('ativo')
+    document.body.style.backgroundColor = menuLateral.classList.contains('ativo') ? '#34495e' : '#ecf0f1'
 });
+background.addEventListener('click', () =>{
+    botao.classList.remove('ativo')
+    menuLateral.classList.remove('ativo')
+    conteudo.classList.remove('ativo')
+    background.classList.remove('ativo')
+    document.body.style.backgroundColor = '#ecf0f1'
+})
